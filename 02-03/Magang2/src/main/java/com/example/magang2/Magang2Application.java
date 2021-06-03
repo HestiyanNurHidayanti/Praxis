7f0d140e85b60ba24ab749cf05b40f1d9d08d7d4
HestiyanNurHidayanti <hestiyan1800016146@webmail.uad.ac.id>
2021-06-03 16:40:43 +0700

    database dan spring

---
+++ 02-03/Magang2/src/main/java/com/example/magang2/Magang2Application.java
@@ -0,0 +1,13 @@
+package com.example.magang2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Magang2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Magang2Application.class, args);
+    }
+
+}
